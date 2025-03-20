c8f873cba3bab6ecf79c03293bd9d3a130954010
rishadpt <rishadpt16@gmail.com>
2025-03-20 14:59:31 +0530

    fix

--- components/atoms/SpecialityTag.tsx
+++ components/atoms/SpecialityTag.tsx
@@ -122,7 +122,7 @@ export default function SpecialityTag({
         variant === 'dark'
           ? 'border-[rgba(0,0,0,0.12)] text-black'
           : 'border-[rgb(146,118,111)] bg-[rgba(0,0,0,0.24)] text-white'
-      } flex gap-[5px] rounded-[16px] border-[1px] px-[5px] py-[3px] items-center backdrop-blur-md`}
+      } flex items-center gap-[5px] rounded-[16px] border-[1px] px-[5px] py-[3px] backdrop-blur-md`}
     >
       {specialtyIcons[tag] ?? null}
       <p className="text-small flex items-center">
